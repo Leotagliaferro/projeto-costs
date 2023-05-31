f6470d5a6d5d1f5b19c79cbf49b4e2eb98d9e4c4
Leonardo Tagliaferro <tagliaferro.leonardo@gmail.com>
2023-05-31 19:55:20 -0400

    projeto estruturado

--- OneDrive/Área de Trabalho/Projeto react/projete-costs/projete-costs/src/App.js
+++ OneDrive/Área de Trabalho/Projeto react/projete-costs/projete-costs/src/App.js
@@ -1,8 +1,43 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import Newproject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/Newproject">Novo projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} > </Route>
+        </Routes>
+        <Routes>
+          <Route path="/Company" element={<Company />} > </Route>
+        </Routes>
+        <Routes>
+         <Route path="/Contact" element={<Contact />} > </Route>
+        </Routes>
+        <Routes>
+          <Route path="/Newproject" element={<Newproject />} > </Route>
+        </Routes>
+        </Container>
+      <p>Footer</p>
+    </Router>
+    
+
+
+  
   );
 }
 
