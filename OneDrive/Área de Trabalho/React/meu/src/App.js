744dcc6296a415d7a723a32b96e848aa999f245e
Leonardo Tagliaferro <tagliaferro.leonardo@gmail.com>
2023-06-09 20:54:27 -0400

    mudança

--- OneDrive/Área de Trabalho/React/meu/src/App.js
+++ OneDrive/Área de Trabalho/React/meu/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Switch, Route, Link, Routes} from 'react-router-dom'
+import Home from './pages/Home';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/empresa">Empresa</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+      </ul>
+      <Switch>
+    <Routes>
+      <Route path="/"element={<Home />}/>
+    </Routes>
+      </Switch>
+    </Routes>  
+    
+    
+  )  
 }
+
 
 export default App;
